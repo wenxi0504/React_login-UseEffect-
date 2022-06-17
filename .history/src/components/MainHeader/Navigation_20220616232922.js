d62893530401f32fd043c44eaec231c18b5aac53
Sophie <wenxi1988@hotmail.com>
2022-06-16 23:37:14 -0400

    Using dynamic useContext hook  (Function)

--- .history/src/components/MainHeader/Navigation_20220616232922.js
+++ .history/src/components/MainHeader/Navigation_20220616232922.js
@@ -3,7 +3,7 @@ import React, { useContext } from "react";
 import classes from "./Navigation.module.css";
 import AuthContext from "../../store/auth-context";
 
-const Navigation = () => {
+const Navigation = (props) => {
   const ctx = useContext(AuthContext);
   return (
     // <AuthContext.Consumer>
